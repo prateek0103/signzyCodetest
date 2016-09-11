8af752cb91213f6d4b69dd59fa86acc21ae3b2f2
Prateek Gupta <aryan.838@gmail.com>
2016-09-11 11:05:29 +0000

    API done

--- api.js
+++ api.js
@@ -1,10 +1,26 @@
 var express = require("express");
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'astronaut',
+    socketPath: "/var/run/mysqld/mysqld.sock",
+    database: 'name'
+});
+
+connection.connect();
+
 var app = express();
 
-app.get("/search/:fname/:lname", function (req, res) {
-    var fname = req.params.fname;
-    var lname = req.params.lname;
-    console.log(fname, lname);
+app.get("/search/:query", function (req, res) {
+    var query = "SELECT * FROM `nameCombinations` WHERE (CONVERT(`username` USING utf8mb4) LIKE '%" + req.params.query + "%') LIMIT 5";
+    console.log(query);
+    connection.query(query, function(err, rows){
+     if(err) throw err;
+     res.status(200).send(rows);
+     //connection.end();
+  });
 });
 
 app.listen(8082, function(err){
